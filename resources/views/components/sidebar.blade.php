860ee0d8c3f0a8a4654ebab14aac8c48ad57c549
Fatma R <anthoschichequs@gmail.com>
2025-06-27 16:40:22 +0300

    sidebar acik tema icin uygun hale getirildi.

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,6 +1,6 @@
 <aside
     x-data="{ openNotes: true, openQuick: true, openNotebooks: true, openTags: true, selected: 'uncategorized' }"
-    class="w-[198px] bg-[#1e2020] border-r border-[#0f0f14] p-2 overflow-y-auto text-gray-300 space-y-2 select-none text-xs">
+    class="w-[198px] bg-white dark:bg-gray-900 border-r border-gray-200 dark:border-gray-800 p-2 overflow-y-auto text-gray-800 dark:text-gray-200 space-y-2 select-none text-xs">
 
     @php
         $total = $notes->count();
@@ -11,48 +11,45 @@
     {{-- ALL NOTES --}}
     <div class="space-y-1">
         <button @click="openNotes = !openNotes"
-                class="flex items-center justify-between w-full hover:text-white px-2 py-2 rounded hover:bg-[#2d2e31] cursor-pointer">
+                class="flex items-center justify-between w-full hover:text[#1a1a1a] dark:hover:text-white px-2 py-2 rounded bg-white hover:bg-[#f9f9f9] dark:hover:bg-gray-700 cursor-pointer">
             <div class="flex items-center gap-1">
-                <svg :class="{ 'rotate-90': openNotes }"
-                     xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                     stroke-width="1.5" stroke="currentColor"
-                     class="size-4 text-gray-400 hover:text-gray-200 transform transition-transform">
+                <svg :class="{ 'rotate-90': openNotes }" class="size-4 text-gray-400 transform transition-transform"
+                     xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5"/>
                 </svg>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1"
-                     stroke="currentColor" class="w-5 h-5">
+                <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round"
                           d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z"/>
                 </svg>
-                <span class="font-bold">All Notes</span>
+                <span class="">All Notes</span>
             </div>
             <span>{{ $total }}</span>
         </button>
 
         <div x-show="openNotes" x-transition class="ml-8">
             <div @click="selected = 'uncategorized'"
-                 :class="selected === 'uncategorized' ? 'bg-[#2d2e31]' : ''"
-                 class="flex items-center gap-2 hover:text-white px-2 py-2 rounded hover:bg-[#2d2e31] cursor-pointer transition">
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                     stroke="currentColor" class="size-4 text-[#26af68]">
+                 :class="selected === 'uncategorized' ? 'bg-[#f4f4f4] dark:bg-gray-700 dark:text-white' : ''"
+                 class="flex items-center gap-2 hover:bg-[#f9f9f9] dark:hover:text-white px-2 py-2 rounded bg-white dark:hover:bg-gray-700 cursor-pointer transition">
+                <svg class="size-4 text-green-600 dark:text-green-400" xmlns="http://www.w3.org/2000/svg"
+                     fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round"
                           d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 0 1-.659 1.591l-5.432 5.432a2.25 2.25 0 0 0-.659 1.591v2.927a2.25 2.25 0 0 1-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 0 0-.659-1.591L3.659 7.409A2.25 2.25 0 0 1 3 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0 1 12 3Z"/>
                 </svg>
-                <span class="">Uncategorized</span>
+                <span>Uncategorized</span>
                 <span>{{ $uncategorized }}</span>
             </div>
             <div @click="selected = 'todo'"
-                 :class="selected === 'todo' ? 'bg-[#2d2e31]' : ''"
-                 class="flex items-center gap-2 hover:text-white px-2 py-2 rounded hover:bg-[#2d2e31] cursor-pointer transition">
-                <x-antdesign-check-square-o class="h-4 w-4 text-[#cf6a6a]"/>
-                <span class="">Todo</span>
+                 :class="selected === 'todo' ? 'bg-[#f4f4f4] dark:bg-gray-700 dark:text-white' : ''"
+                 class="flex items-center gap-2 hover:bg-[#f9f9f9] dark:hover:text-white px-2 py-2 rounded bg-white dark:hover:bg-gray-700 cursor-pointer transition">
+                <x-antdesign-check-square-o class="h-4 w-4 text-red-500 dark:text-red-400"/>
+                <span>Todo</span>
                 <span>{{ $todo }}</span>
             </div>
             <div @click="selected = 'unsynced'"
-                 :class="selected === 'unsynced' ? 'bg-[#2d2e31]' : ''"
-                 class="flex items-center gap-2 hover:text-white px-2 py-2 rounded hover:bg-[#2d2e31] cursor-pointer transition">
-                <x-coolicon-cloud-off class="h-4 w-4 text-[#b867a3]"/>
-                <span class="">Unsynced</span>
+                 :class="selected === 'unsynced' ? 'bg-[#f4f4f4] dark:bg-gray-700 dark:text-white' : ''"
+                 class="flex items-center gap-2 hover:bg-[#f9f9f9] dark:hover:text-white px-2 py-2 rounded bg-white dark:hover:bg-gray-700 cursor-pointer transition">
+                <x-coolicon-cloud-off class="h-4 w-4 text-pink-500 dark:text-pink-400"/>
+                <span>Unsynced</span>
             </div>
         </div>
     </div>
@@ -60,15 +57,15 @@
     {{-- QUICK ACCESS --}}
     <div class="space-y-1">
         <button @click="openQuick = !openQuick"
-                class="flex items-center justify-between w-full hover:text-white px-2 py-2 rounded hover:bg-[#2d2e31] cursor-pointer">
+                class="flex items-center justify-between w-full hover:text-white px-2 py-2 rounded  dark:hover:bg-gray-700 cursor-pointer">
             <div class="flex items-center gap-1">
                 <svg :class="{ 'rotate-90': openQuick }"
+                     class="size-4 text-gray-400 transform transition-transform"
                      xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                     stroke-width="1.5" stroke="currentColor"
-                     class="size-4 text-gray-400 hover:text-gray-200 transform transition-transform">
+                     stroke-width="1.5" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5"/>
                 </svg>
-                <span class="text-[#4a91fc] font-bold "> Quick Access</span>
+                <span class="text-[#037ac6] dark:text-blue-400 font-bold">Quick Access</span>
             </div>
         </button>
         <div x-show="openQuick" x-transition class="ml-6 space-y-2">
@@ -79,17 +76,17 @@
     {{-- NOTEBOOKS --}}
     <div class="space-y-1">
         <button @click="openNotebooks = !openNotebooks"
-                class="flex items-center justify-between w-full hover:text-white px-2 py-2 rounded hover:bg-[#2d2e31] cursor-pointer">
+                class="flex items-center justify-between w-full hover:text-white px-2 py-2 rounded dark:hover:bg-gray-700 cursor-pointer">
             <div class="flex items-center gap-1">
                 <svg :class="{ 'rotate-90': openNotebooks }"
+                     class="size-4 text-gray-400 transform transition-transform"
                      xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                     stroke-width="1.5" stroke="currentColor"
-                     class="size-4 text-gray-400 hover:text-gray-200 transform transition-transform">
+                     stroke-width="1.5" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5"/>
                 </svg>
-                <span class="text-[#4a91fc] font-bold">Notebooks</span>
+                <span class="text-[#037ac6] dark:text-blue-400 font-bold">Notebooks</span>
             </div>
-            <span class="pb-1 text-[15px] text-[#4a91fc] hover:text-[#2b568f]">+</span>
+            <span class="pb-1 text-[15px] text-[#037ac6] dark:text-blue-400 hover:text-blue-700">+</span>
         </button>
         <div x-show="openNotebooks" x-transition class="ml-6 space-y-2">
             <div class="text-gray-500 text-xs italic px-2">No notebooks yet.</div>
@@ -99,15 +96,15 @@
     {{-- TAGS --}}
     <div class="space-y-1">
         <button @click="openTags = !openTags"
-                class="flex items-center justify-between w-full hover:text-white px-2 py-2 rounded hover:bg-[#2d2e31] cursor-pointer">
+                class="flex items-center justify-between w-full hover:text-white px-2 py-2 rounded dark:hover:bg-gray-700 cursor-pointer">
             <div class="flex items-center gap-1">
                 <svg :class="{ 'rotate-90': openTags }"
+                     class="size-4 text-gray-400 transform transition-transform"
                      xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                     stroke-width="1.5" stroke="currentColor"
-                     class="size-4 text-gray-400 hover:text-gray-200 transform transition-transform">
+                     stroke-width="1.5" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5"/>
                 </svg>
-                <span class="text-[#4a91fc] font-bold ">Tags</span>
+                <span class="text-[#037ac6] dark:text-blue-400 font-bold">Tags</span>
             </div>
         </button>
         <div x-show="openTags" x-transition class="ml-6 space-y-2">
@@ -115,16 +112,17 @@
         </div>
     </div>
 
-    {{-- DİĞER KISIMLAR --}}
-    <div class="flex items-center justify-between hover:text-white px-2 py-2 rounded cursor-pointer ml-5">
+    {{-- TEMPLATES & TRASH --}}
+    <div class="flex items-center justify-between hover:text-white px-2 py-2 rounded cursor-pointer ml-5 dark:hover:bg-gray-700">
         <div class="flex items-center gap-1">
-            <span class="text-[#4a91fc] font-bold ">Templates</span>
+            <span class="text-[#037ac6] dark:text-blue-400 font-bold">Templates</span>
         </div>
     </div>
 
-    <div class="flex items-center justify-between hover:text-white px-2 py-1 rounded cursor-pointer ml-5">
+    <div class="flex items-center justify-between hover:text-white px-2 py-2 rounded cursor-pointer ml-5  dark:hover:bg-gray-700">
         <div class="flex items-center gap-1">
-            <span class="text-[#4a91fc] font-bold ">Trash</span>
+            <span class="text-[#037ac6] dark:text-blue-400 font-bold">Trash</span>
         </div>
     </div>
+
 </aside>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -1,8 +1,8 @@
-<nav class="h-9 bg-white dark:bg-[#1e2020] text-[#707070] dark:text-gray-200 border-b border-[#0f0f14] flex items-center justify-between px-3">
+<nav class="h-9 bg-white dark:bg-[#1e2020] text-[#707070] dark:text-gray-200 border-b border-[#e7e7e7] dark:border-[#0f0f14] flex items-center justify-between px-3">
     {{-- Sol taraf: Menü, Geri/İleri, Arama --}}
     <div class="flex items-center">
         <!-- Menü ikonu -->
-        <button class="hover:bg-gray-800 p-1 rounded mr-2">
+        <button class="hover:text-gray-800 p-1 rounded mr-2">
             <svg class="w-4 h-4" fill="none" stroke="currentColor" stroke-width="1.5"
                  viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
@@ -10,7 +10,7 @@
         </button>
 
         <!-- Geri -->
-        <button class="hover:bg-gray-800 p-1 rounded">
+        <button class="hover:text-gray-800 p-1 rounded">
             <svg class="w-5 h-5" fill="none" stroke="currentColor" stroke-width="1.5"
                  viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M15 19l-7-7 7-7" />
@@ -18,7 +18,7 @@
         </button>
 
         <!-- İleri -->
-        <button class="hover:bg-gray-800 p-1 rounded mr-3">
+        <button class="hover:text-gray-800 p-1 rounded mr-3">
             <svg class="w-5 h-5" fill="none" stroke="currentColor" stroke-width="1.5"
                  viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
@@ -44,7 +44,7 @@
         <x-button>New Note</x-button>
 
         <!-- Kopyalama -->
-        <button class="hover:bg-gray-800 p-1 rounded">
+        <button class="hover:text-gray-800 p-1 rounded">
             <svg class="w-5 h-5" fill="none" stroke="currentColor" stroke-width="1.5"
                  viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path stroke-linecap="round" stroke-linejoin="round"
@@ -53,7 +53,7 @@
         </button>
 
         <!-- Ayarlar -->
-        <button class="hover:bg-gray-800 p-1 rounded">
+        <button class="hover:text-gray-800 p-1 rounded">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M10.343 3.94c.09-.542.56-.94 1.11-.94h1.093c.55 0 1.02.398 1.11.94l.149.894c.07.424.384.764.78.93.398.164.855.142 1.205-.108l.737-.527a1.125 1.125 0 0 1 1.45.12l.773.774c.39.389.44 1.002.12 1.45l-.527.737c-.25.35-.272.806-.107 1.204.165.397.505.71.93.78l.893.15c.543.09.94.559.94 1.109v1.094c0 .55-.397 1.02-.94 1.11l-.894.149c-.424.07-.764.383-.929.78-.165.398-.143.854.107 1.204l.527.738c.32.447.269 1.06-.12 1.45l-.774.773a1.125 1.125 0 0 1-1.449.12l-.738-.527c-.35-.25-.806-.272-1.203-.107-.398.165-.71.505-.781.929l-.149.894c-.09.542-.56.94-1.11.94h-1.094c-.55 0-1.019-.398-1.11-.94l-.148-.894c-.071-.424-.384-.764-.781-.93-.398-.164-.854-.142-1.204.108l-.738.527c-.447.32-1.06.269-1.45-.12l-.773-.774a1.125 1.125 0 0 1-.12-1.45l.527-.737c.25-.35.272-.806.108-1.204-.165-.397-.506-.71-.93-.78l-.894-.15c-.542-.09-.94-.56-.94-1.109v-1.094c0-.55.398-1.02.94-1.11l.894-.149c.424-.07.765-.383.93-.78.165-.398.143-.854-.108-1.204l-.526-.738a1.125 1.125 0 0 1 .12-1.45l.773-.773a1.125 1.125 0 0 1 1.45-.12l.737.527c.35.25.807.272 1.204.107.397-.165.71-.505.78-.929l.15-.894Z" />
                 <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
